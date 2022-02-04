02f90a4d98dfc5f869539ec46f83e6b265c35bcc
noah figueras <noahfigueras@gmail.com>
2022-02-03 20:31:22 -0800

    Fix: indentation problem

--- contracts/BulkRegistrar.sol
+++ contracts/BulkRegistrar.sol
@@ -31,24 +31,23 @@ contract BulkRegistrar {
 				total += controller.rentPrice(names[i], duration);
 		}
 	}
-
+		
 	function submitCommit(string[] calldata names, address owner, bytes32 secret) external {
 		IETHRegistrarController controller = getController();
 		for(uint i = 0; i < names.length; i++) {
-      bytes32 commitment = controller.makeCommitment(names[i],owner,secret);
-      controller.commit(commitment);
-    }
+      		bytes32 commitment = controller.makeCommitment(names[i],owner,secret);
+      		controller.commit(commitment);
+    	}
 	}
 
 	function registerAll(string[] calldata names, address owner, uint duration, bytes32 secret) external payable {
 		require(owner == msg.sender, "Error: Caller must be the same address as owner");
 		IETHRegistrarController controller = getController();	
 		for(uint i = 0; i < names.length; i++) {
-				uint cost = controller.rentPrice(names[i], duration);
-				controller.register{value:cost}(names[i], owner, duration, secret);
+			uint cost = controller.rentPrice(names[i], duration);
+			controller.register{value:cost}(names[i], owner, duration, secret);
 		}
 		// Send any excess funds back
 		payable(msg.sender).transfer(address(this).balance);
 	}
- 
 }
