af31a1619402d21998a0fe91fbb8d1c1ad33b451
noah figueras <noahfigueras@gmail.com>
2022-02-03 16:02:45 -0800

    Added: Smart contract compiled correctly with bulkRentPrice and bulkRegistering

--- contracts/BulkRegistrar.sol
+++ contracts/BulkRegistrar.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 interface IETHRegistrarController {
 	function available(string memory name) external view returns(bool);
+	function register(string calldata name, address owner, uint duration, bytes32 secret) external payable;
+	function rentPrice(string memory name, uint duration) external view returns(uint);
 }
 
 contract BulkRegistrar {
@@ -17,7 +19,26 @@ contract BulkRegistrar {
 	} 
 
 	function available (string memory name) external view returns(bool) {
-		 IETHRegistrarController controller = getController();	
-		 return controller.available(name);	
+		IETHRegistrarController controller = getController();	
+	 	return controller.available(name);	
 	}
+
+	function rentPrice(string[] calldata names, uint duration) external view returns(uint total) {
+		IETHRegistrarController controller = getController();
+		for(uint i = 0; i < names.length; i++) {
+				total += controller.rentPrice(names[i], duration);
+		}
+	}
+
+	function registerAll(string[] calldata names, address owner, uint duration, bytes32 secret) external payable {
+		require(owner == msg.sender, "Error: Caller must be the same address as owner");
+		IETHRegistrarController controller = getController();	
+		for(uint i = 0; i < names.length; i++) {
+				uint cost = controller.rentPrice(names[i], duration);
+				controller.register{value:cost}(names[i], owner, duration, secret);
+		}
+		// Send any excess funds back
+		payable(msg.sender).transfer(address(this).balance);
+	}
+ 
 }
