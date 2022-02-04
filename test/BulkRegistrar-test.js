ae97bb21694305793c0be871be2d00b92012b20f
noah figueras <noahfigueras@gmail.com>
2022-02-03 20:14:58 -0800

    Tests: All pass correctly and registers in bulk work successfully

--- test/BulkRegistrar-test.js
+++ test/BulkRegistrar-test.js
@@ -4,6 +4,8 @@ describe("Registering ens domains in bulk", function() {
   let Contract;
   let contract;
   let owner;
+	const duration = 31536000; // 1year in seconds
+  const delay = ms => new Promise(res => setTimeout(res, ms)); // Utility function
 
   beforeEach( async () => {
 		const ETHRegistrarControllerAddress = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'; // Mainnet
@@ -20,7 +22,46 @@ describe("Registering ens domains in bulk", function() {
 
     expect(noah).to.equal(false);
     expect(noahFigueras).to.equal(true);
-	
   });
 
+  it("Returns price of registerAll domains", async function() {
+		const domains = ["noahfigueras", "lokoperez", "elzorroloko"];
+		const rentPrice = await contract.rentPrice(domains, duration);
+		const totalCost = ethers.utils.formatEther(rentPrice);	
+	});
+
+  it("Reverts array of domains when one it's not available", async function() {
+		const domains = ["noahfigueras", "noah", "batmabnxoxo"];
+		const secret = ethers.utils.formatBytes32String("lala123lsdkfjalkjerklasdjklj");
+		await contract.submitCommit(domains, owner.address, secret);
+	
+		// Protocol has to wait to verify commit to prevent front run
+		await delay(65000); 
+
+    let revert = false;
+    try{
+		  await contract.registerAll(domains, owner.address , duration, secret, {value: ethers.utils.parseEther("1.0")});
+    } catch(err){
+     revert = true; 
+    }
+    expect(revert).to.equal(true);
+	});
+
+  it("Registers domains in bulk", async function() {
+		const domains = ["noahfigueras", "batmansuper", "freaksworld"];
+		const secret = ethers.utils.formatBytes32String("lalalsdkfjalkjerklasdjklj");
+		await contract.submitCommit(domains, owner.address, secret);
+	
+		// Protocol has to wait to verify commit to prevent front run
+		await delay(65000); 
+    // Register bulk
+		await contract.registerAll(domains, owner.address , duration, secret, {value: ethers.utils.parseEther("1.0")});
+    // Check
+	  const noahFigueras = await contract.available("noahfigueras");	
+	  const freaksWorld = await contract.available("freaksworld");	
+	  const batmanSuper = await contract.available("batmansuper");	
+    expect(noahFigueras).to.equal(false);
+    expect(freaksWorld).to.equal(false);
+    expect(batmanSuper).to.equal(false);
+	});
 });
