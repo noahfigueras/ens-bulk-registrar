f37c4fd16ca9961677d3405cfdeeff0a374af8ad
noah figueras <noahfigueras@gmail.com>
2022-02-07 10:54:46 -0800

    Added: Estimation of gas usage on deploy contract + calculate percentage of gas savings

--- scripts/estimateGas.js
+++ scripts/estimateGas.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
 
-async function main() {
+async function main(args) {
 	if(hre.network.name != "hardhat") {
 		console.log("Error: Please run only on hardhat network with fork");
 		return 1;
@@ -27,19 +27,26 @@ async function main() {
 	// Deploying main contract
 	Contract = await ethers.getContractFactory("BulkRegistrar");
 	contract = await Contract.deploy(ETHRegistrarControllerAddress);
-	await contract.deployed();
+	const deployed = await contract.deployTransaction.wait();
+	console.log("Gas Estimation for deployment of contract:", Number(deployed.gasUsed));
 
 	// Set constants
+	let total;
+	let saved;
 	const Ether = ethers.utils.parseEther("1.0");
-	const domains = ["noahfigueras", "batmansuper", "freaksworld", "lolomartinez"];
+	const number = 4;
+	const duration = 31536000; // 1year in seconds
+	const domains = generateDomains(number);
 	const secret = ethers.utils.formatBytes32String("supersecretpassword");
 
 	// Estimate gas for commit 
 	const commit = await mainnetContract.makeCommitment('noahfiguerasmartinez', owner.address, secret);	
 	const one = await mainnetContract.estimateGas.commit(commit);
 	const two = await contract.estimateGas.submitCommit(domains, owner.address, secret);
+	total = Number(one) * number;
+	saved = Number(two);
 	console.log("Gas Estimate for every single commit with original contract:", Number(one));
-	console.log("Gas Estimate for 4 bulk commit with new contract:", Number(two));
+	console.log(`Gas Estimate for ${number} bulk commit with new contract:`, Number(two));
 
 	// Commit realistically for registering to work
 	await mainnetContract.commit(commit);
@@ -49,11 +56,31 @@ async function main() {
 	await delay(65000);
 
 	// Estimage gas for registering domains
-	const duration = 31536000; // 1year in seconds
 	const first = await mainnetContract.estimateGas.register("noahfiguerasmartinez", owner.address, duration, secret, {value: Ether});
 	const second = await contract.estimateGas.registerAll(domains, owner.address, duration, secret, {value: Ether});
+	total = total + (Number(first) * number); 
+	saved = saved + Number(second);
 	console.log("Gas Estimate for every single register with original contract:", Number(first));
-	console.log("Gas Estimate for registering 4 domains on bulk with new contract:", Number(second));
+	console.log(`Gas Estimate for registering ${number} domains on bulk with new contract:`, Number(second));
+
+	// Calculate Percentages
+	const perc = calcPercentage(total, saved);		
+	console.log(`Percentage of gas saved is ${perc}`);
+}
+
+function generateDomains(num) {
+	const name = 'noahfigueras';
+	const arr = [];
+	for(let i = 0; i < num; i++) {
+		let n = name + String(i);
+		arr.push(n);
+	}
+	return arr;
+}
+
+function calcPercentage(total, saved) {
+	const savings = total - saved;
+	return (savings / total) * 100;
 }
 
 main()
