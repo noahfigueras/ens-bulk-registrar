1bfc6fb638c37d1521cd86f998277a990b93c1a7
noah figueras <noahfigueras@gmail.com>
2022-03-04 20:16:10 -0800

    Update: tests to match contract with resolver implementation

--- test/BulkRegistrar-test.js
+++ test/BulkRegistrar-test.js
@@ -8,11 +8,11 @@ describe("Registering ens domains in bulk", function() {
 	const delay = ms => new Promise(res => setTimeout(res, ms)); // Utility function
 	const Ether = ethers.utils.parseEther("1.0");
 	const ownerFee = ethers.utils.parseEther("0.01");
+	const resolver = "0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41"; // Mainnet
 
 	beforeEach( async () => {
-		const ETHRegistrarControllerAddress = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'; // Mainnet
 		Contract = await ethers.getContractFactory("BulkRegistrar");
-		contract = await Contract.deploy(ETHRegistrarControllerAddress);
+		contract = await Contract.deploy();
 		await contract.deployed();
 
 		[owner, user] = await ethers.getSigners();
@@ -35,14 +35,14 @@ describe("Registering ens domains in bulk", function() {
 	it("Reverts array of domains when one it's not available", async function() {
 		const domains = ["noahfigueras", "noah", "batmabnxoxo"];
 		const secret = ethers.utils.formatBytes32String("lalalsdkfjalkjerklasdjklj");
-		await contract.submitCommit(domains, owner.address, secret);
+		await contract.submitCommit(domains, owner.address, secret, resolver, owner.address);
 
 		// Protocol has to wait to verify commit to prevent front run
 		await delay(65000); 
 
 		let revert = false;
 		try{
-			await contract.registerAll(domains, owner.address , duration, secret, {value: Ether});
+			await contract.registerAll(domains, owner.address , duration, secret,resolver, owner.address, {value: Ether});
 		} catch(err){
 			revert = true; 
 		}
@@ -52,13 +52,13 @@ describe("Registering ens domains in bulk", function() {
 	it("Registers domains in bulk", async function() {
 		const domains = ["noahfigueras", "batmansuper", "freaksworld", "noahfigueroa0", "noahfigueroa1", "noahfigueroa"];
 		const secret = ethers.utils.formatBytes32String("supersecretpassword");
-		await contract.submitCommit(domains, owner.address, secret);
+		await contract.submitCommit(domains, owner.address, secret, resolver, owner.address);
 
 		// Protocol has to wait to verify commit to prevent front run
 		await delay(65000); 
 		// Register bulk
-		const estimate = await contract.estimateGas.registerAll(domains, owner.address, duration, secret, {value: Ether});
-		await contract.registerAll(domains, owner.address , duration, secret, {value: Ether, gasLimit: estimate});
+		const estimate = await contract.estimateGas.registerAll(domains, owner.address, duration, secret, resolver, owner.address, {value: Ether});
+		await contract.registerAll(domains, owner.address, duration, secret, resolver, owner.address, {value: Ether, gasLimit: estimate});
 		// Check
 		const noahFigueras = await contract.available("noahfigueras");	
 		const freaksWorld = await contract.available("freaksworld");	
@@ -71,14 +71,14 @@ describe("Registering ens domains in bulk", function() {
 	it("Transfers fee to owner of contract with flat fee option", async function() {
 		const domains = ["lokoPerez", "batmansuper2", "freaksworld2"];
 		const secret = ethers.utils.formatBytes32String("supersecretpassword");
-		await contract.connect(user).submitCommit(domains, user.address, secret);
+		await contract.connect(user).submitCommit(domains, user.address, secret, resolver, owner.address);
 		await contract.changeFeeStyle(true);
 
 		// Protocol has to wait to verify commit to prevent front run
 		await delay(65000); 
 		// Register bulk
 		const beforeBalance = await owner.getBalance();
-		await contract.connect(user).registerAll(domains, user.address , duration, secret, {value: Ether});
+		await contract.connect(user).registerAll(domains, user.address , duration, secret,resolver, owner.address, {value: Ether});
 		const afterBalance = await owner.getBalance();
 		
 		const resultBalance = beforeBalance.add(ownerFee);
@@ -88,15 +88,15 @@ describe("Registering ens domains in bulk", function() {
 	it("Transfers fee to owner of contract with percentage fee option", async function() {
 		const domains = ["lokoPerez10", "batmansuper20", "freaksworld20", "noahfigueras10"];
 		const secret = ethers.utils.formatBytes32String("supersecretpassword");
-		await contract.connect(user).submitCommit(domains, user.address, secret);
+		await contract.connect(user).submitCommit(domains, user.address, secret, resolver, owner.address);
 		await contract.changeFeeStyle(false);
 
 		// Protocol has to wait to verify commit to prevent front run
 		await delay(65000); 
 		// Register bulk
-		const estimate_gas = await contract.connect(user).estimateGas.registerAll(domains, user.address , duration, secret, {value: Ether});
+		const estimate_gas = await contract.connect(user).estimateGas.registerAll(domains, user.address , duration, secret, resolver, owner.address, {value: Ether});
 		const beforeBalance = await owner.getBalance();
-		const tx = await contract.connect(user).registerAll(domains, user.address , duration, secret, {value: Ether, gasLimit: estimate_gas});
+		const tx = await contract.connect(user).registerAll(domains, user.address , duration, secret, resolver, owner.address, {value: Ether, gasLimit: estimate_gas.add("10000")});
 		const afterBalance = await owner.getBalance();
 
 		const gasFees = estimate_gas.mul(tx.gasPrice);
@@ -104,7 +104,7 @@ describe("Registering ens domains in bulk", function() {
 		const returned = gasFees.mul(perc).div(ethers.BigNumber.from('100'));
 		const diff = afterBalance.sub(beforeBalance);
 
-		// check if result has a margin of max 70000 gwei
+		// check if result has a margin of max 70000 gwei (down to 18 cents difference at the time) 
 		let revert = false;
 		const max = ethers.utils.parseUnits("70000.0", "gwei");
 		if(returned.sub(diff) > max) {
